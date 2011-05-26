49f84f6d87ec0fe47b81740de7fa565a6923ea98
Andrew Duhan <andrewduhan@gmail.com>
2011-05-26 16:48:22 -0500

    kiosk UI nearly done, JS validations, changed Team behavior, basic kiosk permissions, code cleanup & improvements, initial kiosk sessions work

--- app/models/vms/user.rb
+++ app/models/vms/user.rb
@@ -1,18 +1,24 @@
 class User < ActiveRecord::Base
-  def vms_active_sites
+  def vms_active_scenario_sites
     Vms::ScenarioSite.find(:all, :conditions => ["site_admin_id = ? AND status = ? AND (select status from vms_scenarios where scenario_id = scenario_id) = ?", 
                                                  id, Vms::ScenarioSite::STATES[:active],Vms::Scenario::STATES[:executed] ])
   end
 
-  def is_vms_active_site_admin?
-    vms_active_sites.count > 0
+  def is_vms_active_scenario_site_admin?
+    vms_active_scenario_sites.count > 0
   end
 
-  def vms_sites
+  def vms_scenario_sites
     Vms::ScenarioSite.find(:all, :conditions => {:site_admin_id => id})
   end
-  
-  def is_vms_site_admin?
-    vms_sites.count > 0
+
+  def is_vms_scenario_site_admin?
+    vms_scenario_sites.count > 0
   end
+
+  def is_vms_scenario_site_admin_for?(scenario_site)
+    scenario_site.site_admin_id == id
+  end
+
+
 end
--- public/javascripts/kiosk/kiosk_check_in.js
+++ public/javascripts/kiosk/kiosk_check_in.js
@@ -2,10 +2,8 @@ Ext.ns("Talho.VMS");
 
 (function(){
 
-  var TARGET_DIV_ID = 'checkin-ext-wrapper';
-
   var getAvailableHeight = function(){
-    return Ext.getDoc().getViewSize(false).height - Ext.get('vms-kiosk-header').getHeight();
+    return Ext.getDoc().getViewSize(false).height - Ext.get('vms-kiosk-header').getHeight() -10;
   };
   
   var doResize = function() {
@@ -13,83 +11,243 @@ Ext.ns("Talho.VMS");
   };
 
   var kiosk = Ext.extend(Ext.util.Observable, {
-     init: function(){
+
+    clearPassword: function(){ this.signinPasswordField.setValue(null); },
+
+    clearEmail: function(){ this.signinEmailField.setValue(null); },
+
+    setCheckinButtonLabel: function(label_text){
+      this.checkInOutButton.setText('<span style="font-size: 150%; font-weight: bold;">&nbsp;&nbsp;' + label_text +  '&nbsp;&nbsp;</span>')
+    },
+
+    registeredCheckin: function(form){
+      this.checkInOutButton.disable();
+      var params = {'scenario_site_id':SCENARIO_SITE_ID};
+      var record = this.volunteersList.getSelectionModel().getSelected();
+      if (record != null && record.data['type'] == 'walkup' ){
+        params['walkup_signout'] = true;
+        params['walkup_id'] = record['id'];
+      }
+      form.getForm().submit({
+        params: params,
+        scope: this,
+        success: function(){
+         this.signInForm.getForm().reset();
+         this.checkInOutButton.enable();
+         this.siteVolunteersStore.load();
+        },
+        failure: function(){
+         this.clearPassword();
+         this.checkInOutButton.enable();
+         Ext.Msg.alert('Error', 'Invalid Email or Password. <br> Please try again.');
+        }
+      })
+    },
+
+    invalidBlankField: function(text_field){ // returns false if the field is blank
+      if (text_field.getValue() == ''){
+        text_field.markInvalid('Required');
+        return false;
+      } else {
+        return true;
+      }
+    },
+
+    walkupCheckin: function(form, new_user_form){
+      //manual validation here because of some timing weirdness with resetForm/clearInvalid and allowBlank:true
+      var valid = (this.invalidBlankField(this.walkupFirstNameField) & this.invalidBlankField(this.walkupLastNameField));
+      var params = {};
+      if ( this.walkupNewUserCheckbox.getValue() ){
+        valid = (this.invalidBlankField(this.walkupPasswordField) & this.invalidBlankField(this.walkupPasswordConfirmField) & this.invalidBlankField(this.walkupEmailField));
+        params = new_user_form.getForm().getValues();
+      }
+      if (valid) {
+        this.walkupCheckinButton.disable();
+        params['scenario_site_id'] = SCENARIO_SITE_ID;
+        form.getForm().submit({
+          scope: this,
+          params: params,
+          success: function(){
+            this.walkupForm.getForm().reset();
+            this.walkupCheckinButton.enable();
+            this.siteVolunteersStore.load();
+            Ext.Msg.alert('Success', 'You have been checked in.<br> Please remember to check out before you leave.');
+          },
+          failure: function(){
+            this.walkupCheckinButton.enable();
+            Ext.Msg.alert('Error', "We're sorry, an error has occured");
+          }
+        })
+      }
+    },
+
+    setSigninFormMode: function(mode){
+      switch(mode){
+        case 'walkup':
+          this.signinEmailField.setValue('Walk-Up Volunteer');
+          this.signinPasswordField.setValue('no password');
+          this.signinEmailField.disable();
+          this.signinPasswordField.disable();
+          this.resetCheckinButton.show();
+          break;
+        default:
+          this.signinEmailField.enable();
+          this.signinPasswordField.enable();
+          this.resetCheckinButton.hide();
+      }
+    },
+
+    init: function(){
+
       this.siteVolunteersStore = new Ext.data.JsonStore ({
-        url: '/vms_kiosks/1.json',
-        restful: true,
-        root: 'volunteers',
-        fields: ['display_name', 'id', 'image', 'email'],
-        autoLoad: true
-      });
-
-      this.signinEmailField = new Ext.form.TextField({ id:'email', name:'email', inputType:'text', fieldLabel:'Email Address', height: 35, anchor:'100%', style:{'fontSize':'150%'} });
-      this.signinPasswordField = new Ext.form.TextField({ id:'password', name:'password', inputType:'password', fieldLabel:'Password', height: 35, anchor:'100%', style:{'fontSize':'150%'}  });
-
-      this.unassignedSignInForm = new Ext.FormPanel({
-        region: 'south',
-        border: false,
-        style: {'backgroundColor': '#DFE8F6'},
-
-        margins: '10 10 10 10',
-        items: [
-          new Ext.form.DisplayField({ html: 'Please check in with your TxPhin Account', style:{'font-size':'150%'}, hideLabel: true}) ,
-          this.signinEmailField,
-          this.signinPasswordField
-        ],
-        buttonAlign: 'center',
-        buttons: [
-          new Ext.Button({text: '<span style="font-size: 150%; font-weight: bold;">&nbsp;&nbsp;Check In&nbsp;&nbsp;</span>', scale: 'large'})
-        ]
+        url: document.location.href + '.json', restful: true, root: 'volunteers', autoLoad: true,
+        fields: ['id', 'display_name', 'image', 'email', 'type', 'checked_in', 'scenario_site_admin']
       });
 
       this.volunteerTemplate = new Ext.XTemplate(
-        '<div style="width: 70px; height: 55px; float:left;"><img src="{image}"></div><div style="float: left; padding-top: 20px; font-weight: bold; font-size: 200%;">{display_name}</div>'
+        '<div style="width: 70px; height: 55px; float:left;"><img src="{image}"></div>' +
+        '<div style="float: left; padding-top: 20px; font-weight: bold; font-size: 200%;">{display_name}<tpl if="scenario_site_admin"> - <span style="color: green;">Site Admin</span></tpl></div>' +
+        '<div style="float: right;"><img src="/stylesheets/vms/images/check-box<tpl if="checked_in">-checked</tpl>.png"></div>'
       );
 
       this.volunteersList = new Ext.grid.GridPanel ({
         store: this.siteVolunteersStore,
-        autoExpandColumn: 'volunteer',
-        region: 'center',
-        hideHeaders: true,
-        border: false,
-        loadMask: true,
+        loadMask: true, region: 'center', itemId:'volunteers_list', border: false, hideHeaders: true, autoExpandColumn: 'volunteer', trackMouseOver: false,
+        selModel: new Ext.grid.RowSelectionModel({ singleSelect: true }),
         colModel: new Ext.grid.ColumnModel({
           columns: [ { id: 'volunteer', dataIndex: 'id', sortable: false, xtype: 'templatecolumn', tpl: this.volunteerTemplate} ]
         }),
+        viewConfig: {
+          forceFit: true,
+          getRowClass: function(record, index) {
+            if (record.get('checked_in')) { return 'vms-checked-in'; }
+          }
+        },
         listeners: {
           scope: this,
           'rowclick': function(grid, row, column, e){
             var record = grid.getStore().getAt(row);  // Get the Record
             this.signinEmailField.setValue(record.data['email']);
+            this.clearPassword();
+            if (record.data['checked_in']){ this.setCheckinButtonLabel('Check Out'); } else { this.setCheckinButtonLabel('Check In'); }
+            if (record.data['type'] == 'walkup'){ this.setSigninFormMode('walkup'); } else { this.setSigninFormMode('registered'); }
+            this.walkupForm.getForm().reset();
           }
         }
+      });                                                             
+
+      this.signinEmailField = new Ext.form.TextField({
+        id:'email', name:'email', inputType:'text', fieldLabel:'Email Address', enableKeyEvents: true, labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'95%', style:{'fontSize':'150%'},
+        listeners: {
+          scope: this,
+          'keypress': function(){ this.volunteersList.getSelectionModel().clearSelections(); this.setCheckinButtonLabel('Check In'); },
+          'focus': function(){ this.walkupForm.getForm().reset(); this.signinEmailField.clearInvalid(); }
+        } });
+
+      this.signinPasswordField = new Ext.form.TextField({
+        id:'password', name:'password', inputType:'password', fieldLabel:'Password', labelStyle: 'text-align: right; font-weight:bold;', height: 35, anchor:'95%', style:{'fontSize':'300%'},
+        listeners: { scope: this, 'focus': function(){ this.walkupForm.getForm().reset(); } }
       });
 
-      this.registeredVolunteersPanel = new Ext.Panel ({
-        layout: 'border',
-        title: "Registered Volunteers",
-        flex: 1,
-        autoScroll: false,
-        items: [this.volunteersList, this.unassignedSignInForm]
+      this.checkInOutButton =  new Ext.Button({text: '<span style="font-size: 150%; font-weight: bold;">&nbsp;&nbsp;Check In&nbsp;&nbsp;</span>',
+        scale: 'large', scope: this, handler: function(){ this.registeredCheckin(this.signInForm); }
       });
 
-      this.walkupSignupPanel = new Ext.Panel ({
-        layout: 'fit',
-        title: "Walk-Up Volunteers",
-        flex: 1,
-        margins: '0 0 0 10',
-        items: [{html: "pong", border: false}]
+      this.resetCheckinButton =  new Ext.Button({text: '<span style="font-size: 150%;">&nbsp;&nbsp;Reset&nbsp;&nbsp;</span>',
+        scale: 'large', scope: this, hidden: true, handler: function(){ this.signInForm.getForm().reset(); }
+      });
+
+      this.signInForm = new Ext.FormPanel({
+        url: '/vms/site_checkin.json', region: 'north', border: false, style: {'padding':'10px 0'}, bodyStyle: 'background-color: #DFE8F6;',
+        items: [
+          new Ext.form.DisplayField({ html: 'Please check in and out with your TxPhin Account', style:{'text-align': 'center', 'font-size':'150%'}, hideLabel: true}) ,
+          this.signinEmailField,
+          this.signinPasswordField
+        ],
+        buttonAlign: 'center',
+        buttons: [ this.checkInOutButton, this.resetCheckinButton ],
+        keys: [{key: Ext.EventObject.RETURN, shift: false, fn: function(){ this.registeredCheckin(this.signInForm) }, scope: this}]
+      });
+
+      this.walkupFirstNameField = new Ext.form.TextField({
+        id:'walkup_first_name', name:'walkup_first_name', inputType:'text', fieldLabel:'First Name', labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'90%', style:{'fontSize':'150%'},
+        listeners: { scope: this, 'focus': function(){ this.walkupFirstNameField.clearInvalid(); this.signInForm.getForm().reset(); } }
+      });
+      this.walkupLastNameField = new Ext.form.TextField({
+        id:'walkup_last_name', name:'walkup_last_name', inputType:'text', fieldLabel:'Last Name', labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'90%', style:{'fontSize':'150%'},
+        listeners: { scope: this, 'focus': function(){ this.walkupLastNameField.clearInvalid();  this.signInForm.getForm().reset(); } }
+      });
+      this.walkupEmailField = new Ext.form.TextField({
+        id:'walkup_email', name:'walkup_email', inputType:'text', fieldLabel:'Email Address', labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'90%', style:{'fontSize':'150%'}, vtype: 'email',
+        listeners: { scope: this, 'focus': function(){ this.signInForm.getForm().reset(); this.walkupEmailField.clearInvalid(); } }
+      });
+      this.walkupNewUserCheckbox = new Ext.form.Checkbox({ boxLabel: 'Create a TxPhin Account?', name: 'walkup_new_account', checked: true,
+        listeners:{ scope: this,
+          'check': function(box,checked){
+            if (checked) {
+              this.walkupNewUserForm.enable();
+            } else {
+              this.walkupNewUserForm.getForm().reset();
+              this.walkupNewUserForm.disable();
+              this.walkupEmailField.clearInvalid();
+            } } } });
+      this.walkupPasswordField = new Ext.form.TextField({
+        id:'walkup_password', name:'walkup_password', inputType:'password', fieldLabel:'New Password', labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'70%', style:{'fontSize':'150%'}, vtype: 'password',
+        listeners: {scope: this, 'focus': function(){ this.walkupPasswordField.clearInvalid(); } }
+      });
+      this.walkupPasswordConfirmField = new Ext.form.TextField({
+        id:'walkup_password_confirm', name:'walkup_password_confirm', inputType:'password', fieldLabel:'Confirm Password', labelStyle: 'text-align: right; font-weight:bold;',
+        height: 35, anchor:'70%', style:{'fontSize':'150%'}, vtype: 'password', initialPassword:'walkup_password',
+        listeners: {scope: this, 'focus': function(){ this.walkupPasswordConfirmField.clearInvalid(); } }
+      });
+      this.walkupCheckinButton =  new Ext.Button({text: '<span style="font-size: 150%; font-weight: bold;">&nbsp;&nbsp;Check In&nbsp;&nbsp;</span>',
+        scale: 'large', scope: this, handler: function(){ this.walkupCheckin(this.walkupForm, this.walkupNewUserForm); }
+      });
+
+      this.walkupNewUserForm = new Ext.FormPanel({
+        border: false, bodyStyle: 'padding: 10px 0',
+        items: [
+          this.walkupPasswordField,
+          this.walkupPasswordConfirmField,
+          new Ext.form.DisplayField({ html: "Passwords must have at least 8 characters, one Capital and one number", style:{'text-align': 'center',  'font-style':'italic'}, hideLabel: true})
+        ]
+      });
+
+      this.walkupForm = new Ext.FormPanel({
+        url: '/vms/site_walkup.json',
+        border: false,
+        items: [
+          new Ext.form.DisplayField({ html: "Don't have a TxPhin Account?  <br>Please enter your name.", style:{'text-align': 'center', 'padding':'10px 0 10px 0', 'font-size':'150%'}, hideLabel: true}),
+          this.walkupFirstNameField,
+          this.walkupLastNameField,
+          this.walkupEmailField,
+          this.walkupNewUserCheckbox,
+          this.walkupNewUserForm
+        ],
+        buttonAlign: 'center',
+        buttons: [ this.walkupCheckinButton ]
+      });
+
+      this.leftPanel = new Ext.Panel ({
+        layout: 'border', title: 'Registered Volunteers', flex: 1.2, autoScroll: false,
+        items: [this.volunteersList, this.signInForm]
+      });
+
+      this.rightPanel = new Ext.Panel ({
+        resize: true, border: true, flex: 0.8, margins: '0 5 0 30',
+        items: [ this.walkupForm ]
       });
 
       this.checkInPanel = new Ext.Panel({
-        layout: 'hbox',
+        layout: 'hbox', id: 'vms-kiosk-main-ext-panel', border: false, padding: 10,
+        renderTo: TARGET_DIV_ID, layoutConfig: {align: 'stretch'},
         height: getAvailableHeight(),
-        renderTo: TARGET_DIV_ID,
-        layoutConfig: {align: 'stretch'},
-        id: 'vms-kiosk-main-ext-panel',
-        border: false,
-        padding: 10,
-        items: [ this.registeredVolunteersPanel, this.walkupSignupPanel ]
+        items: [ this.leftPanel, this.rightPanel ]
       });
     }
   });
