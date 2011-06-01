1fa75c7d18faf82116197f8c4230b4a78439dd26
Andrew Duhan <andrewduhan@gmail.com>
2011-06-01 14:57:47 -0500

    Team assignments, numerous improvements

--- db/migrate/20110518151200_add_checked_in_to_staff.rb
+++ db/migrate/20110518151200_add_checked_in_to_staff.rb
@@ -1,6 +1,6 @@
 class AddCheckedInToStaff < ActiveRecord::Migration
   def self.up
-    add_column :vms_staff, :checked_in, :boolean
+    add_column :vms_staff, :checked_in, :boolean, :default => false
   end
 
   def self.down
