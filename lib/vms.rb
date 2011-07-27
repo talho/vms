8d91a7143a786be01f89de51c3966e67b3017f14
Charles DuBose <cdubose@ubuntu.(none)>
2011-07-27 10:59:48 -0500

    Making it so that a user doesn't need to be a VMS:Admin to be a site admin for a scenario

--- lib/vms.rb
+++ lib/vms.rb
@@ -24,8 +24,11 @@ $menu_config[:vms] = <<EOF
   nav = "{name: 'VMS', items:["
   if current_user.vms_admin?
     nav += "{name: 'Manage Scenarios', tab:{id: 'vms_open_scenario', title:'Manage Scenarios', initializer: 'Talho.VMS.Scenario.Manager'}},
-            {name: 'Volunteer List', tab: {id: 'vms_volunteer_list', title:'Volunteer List', initializer: 'Talho.VMS.Volunteer.List'}},
-            {name: 'Site Administration', win:{id: 'vms_site_admin', title:'Site Administration', initializer: 'Talho.VMS.AdministerSites'}}"
+            {name: 'Volunteer List', tab: {id: 'vms_volunteer_list', title:'Volunteer List', initializer: 'Talho.VMS.Volunteer.List'}}"
+    nav += "," if current_user.vms_volunteer? || current_user.is_vms_active_scenario_site_admin? 
+  end
+  if current_user.is_vms_active_scenario_site_admin?
+    nav += "{name: 'Site Administration', win:{id: 'vms_site_admin', title:'Site Administration', initializer: 'Talho.VMS.AdministerSites'}}"
     nav += "," if current_user.vms_volunteer?
   end
   nav += "{name: 'My Volunteer Profile', tab:{id: 'vms_user_profile', title:'My Volunteer Profile', initializer: 'Talho.VMS.User.Profile'}}" if current_user.vms_volunteer?
