1f91112d273aa22ba5de96d5895dbb32d2422406
Charles DuBose <cdubose@ubuntu.(none)>
2012-01-23 15:45:29 -0600

    Fixing location of rake task and changing the script load order to fix testing problem

--- public/javascripts/script_config.js
+++ public/javascripts/script_config.js
@@ -1,7 +1,7 @@
 
 dominoes.property('vms', '/javascripts/vms');
 dominoes.rule('VMS_Inventory', '$(vms)/inventory/inventory_controller.js $(ext_extensions)/xActionColumn.js ( $(vms)/item_detail_windows.js > $(vms)/inventory/create_and_edit.js )');
-dominoes.rule('VMS_Site', '$(vms)/site/controller.js ( $(vms)/item_detail_windows.js > $(vms)/site/create_and_edit.js ) $(ext_extensions)/DoNotCollapseActive.js $(vms)/site/site_info_window.js');
+dominoes.rule('VMS_Site', '$(vms)/site/controller.js ( $(vms)/item_detail_windows.js > $(vms)/site/create_and_edit.js ) $(ext_extensions)/DoNotCollapseActive.js $(vms)/site/site_info_window.js ');
 dominoes.rule('VMS_Roles', '$(ext_extensions)/xActionColumn.js $(vms)/roles/controller.js ( $(vms)/item_detail_windows.js > $(vms)/roles/create_and_edit.js )');
 dominoes.rule('VMS_Staff', 'UserSelectionGrid $(vms)/staff/controller.js ( $(vms)/item_detail_windows.js > $(vms)/staff/create_and_edit.js )');
 dominoes.rule('VMS_Teams', 'GroupSelectionGrid UserSelectionGrid $(vms)/teams/controller.js ( $(vms)/item_detail_windows.js > $(vms)/teams/create_and_edit.js )');
@@ -15,10 +15,10 @@ dominoes.rule('VMS_User_Profile', 'VMS_User_Profile_Views $(vms)/user/profile/co
 dominoes.rule('VMS_Volunteer_List_Views', '$(vms)/extensions/column_panel.js > $(vms)/model/volunteer.js $(vms)/extensions/action_button.js $(vms)/volunteer/list/view/column_panel.js $(vms)/volunteer/list/view/list_actions.js $(vms)/volunteer/list/view/volunteer_list.js $(vms)/volunteer/list/view/alert.js $(vms)/volunteer/list/view/list.js $(vms)/volunteer/list/view/new_status_check.js $(vms)/volunteer/list/view/status_checks.js $(vms)/volunteer/list/view/status_responders.js $(vms)/volunteer/list/view/volunteer_actions.js VMS_User_Qualifications');
 dominoes.rule('VMS_Volunteer_List', 'VMS_Volunteer_List_Views $(vms)/volunteer/list/controller.js');
 
-Talho.ScriptManager.addInitializer('Talho.VMS.CommandCenter', {js:'$(ext_extensions)/DoNotCollapseActive.js GMap VMS_Site VMS_Roles VMS_Inventory VMS_Staff VMS_Teams VMS_Qualifications VMS_CommandCenter'});
+Talho.ScriptManager.addInitializer('Talho.VMS.CommandCenter', {js:'GMap > $(ext_extensions)/DoNotCollapseActive.js VMS_Site VMS_Roles VMS_Inventory VMS_Staff VMS_Teams VMS_Qualifications VMS_CommandCenter'});
 Talho.ScriptManager.addInitializer('Talho.VMS.CreateAndEditScenario', {js: 'UserSelectionGrid $(vms)/scenario/create_and_edit.js'});
 Talho.ScriptManager.addInitializer('Talho.VMS.ManageScenarios', {js: '$(ext_extensions)/xActionColumn.js $(vms)/scenario/manage_scenarios.js'});
-Talho.ScriptManager.addInitializer('Talho.VMS.Scenario.Manager', {js: 'VMS_Scenario_Manager'});
+Talho.ScriptManager.addInitializer('Talho.VMS.Scenario.Manager', {js: 'GMap VMS_Scenario_Manager'});
 Talho.ScriptManager.addInitializer('Talho.VMS.AdministerSites', {js: '$(ext_extensions)/xActionColumn.js $(vms)/site/administer_sites.js'});
 Talho.ScriptManager.addInitializer('Talho.VMS.User.Profile', {js: 'VMS_User_Profile'});
 Talho.ScriptManager.addInitializer('Talho.VMS.Volunteer.List', {js: 'VMS_Volunteer_List'});
